d4304d616db8b8828dbf98a061c76d32688259b7
Hyslan Silva Cruz <hyslansilva@gmail.com>
2024-06-06 08:25:13 -0300

    #feat: append new file

--- Insert dados.R
+++ Insert dados.R
@@ -2,19 +2,125 @@ library("readxl")
 library("odbc")
 library("DBI")
 library("dplyr")
+library("lubridate")
+library("stringr")
+
+col_mapping_formularios <- c(
+  NUM_OS = "Número OS"
+  ,COD_TSS_OS_FISCALIZADA = "Cod. TSS OS Fiscalizada"
+  ,CAUSA_RESULTADO_OS_FISCALIZADA = "Causa Resultado OS Fiscalizada"
+  ,GRUPO = "Grupo"
+  ,DESCRICAO = "Descrição"
+  ,CODIGO = "Código"
+  ,VALOR = "Valor"
+)
+
+col_mapping_relatorios <- c(
+  NUM_OS = "Número OS"
+  ,COD_UE = "Código UE"
+  ,UN_EXECUTANTE = "Unidade Executante"
+  ,ATC = "ATC"
+  ,SF = "SF"
+  ,ATO = "ATO"
+  ,AS_OS = "AS"
+  ,ST_OS = "Status"
+  ,EQUIPE_TRABALHO = "Equipe de trabalho"
+  ,COD_CONTRATO = "Código Contrato"
+  ,DESC_CONTRATO = "Descrição Contrato"
+  ,COD_TSS = "Código TSS"
+  ,TSS = "TSS"
+  ,CAUSA_RESULTADO = "Causa Resultado"
+  ,COD_MUNICIPIO = "Código Município"
+  ,MUNICIPIO = "Município...16"
+  ,ENDERECO = "Endereço"
+  ,PDE = "PDE"
+  ,DT_COMPETENCIA = "Data de Competência"
+  ,DT_PLANEJAMENTO = "Data de Planejamento"
+  ,DT_INICIO_EXEC = "Data Início Execução"
+  ,DT_FIM_EXEC_OS_FISCALIZADA = "Data Fim Execução"
+  ,COD_TSS_OS_FISCALIZADA = "Cod. TSS OS Fiscalizada"
+  ,COD_CONTRATO_OS_FISCALIZADA = "Código Contrato OS Fiscalizada"
+  ,DESC_CONTRATO_OS_FISCALIZADA = "Descrição Contrato OS Fiscalizada"
+  ,DT_COMPETENCIA_OS_FISCALIZADA = "Data de Competência OS Fiscalizada"
+  ,DT_PLANEJAMENTO_OS_FISCALIZADA = "Data de Planejamento OS Fiscalizada"
+  ,DT_INICIO_EXEC_OS_FISCALIZADA = "Data Início Execução OS Fiscalizada"
+  ,DT_FIM_EXECUCAO = "Data Fim Execução OS Fiscalizada"
+  ,MUNICIPIO_1 = "Município...30"
+  ,SETOR = "Setor"
+  ,ROTA = "Rota"
+  ,QUADRA = "Quadra"
+  ,LOCAL_OS = "Local"
+  ,VILA = "Vila"
+  ,SUB_LOCAL = "SubLocal"
+  ,LONGITUDE = "Longitude"
+  ,LATITUDE = "Latitude"
+)
+
 
 # Estabeleça a conexão com o banco de dados SQL Server usando Windows Authentication
-conexao <- dbConnect(odbc::odbc(), .connection_string = paste0("Driver={SQL Server};Server=10.66.42.188;Database=BD_MLG;Trusted_Connection=yes;"))
+conexao <- dbConnect(
+  odbc::odbc(),
+  .connection_string = paste0(
+    "Driver={SQL Server};Server=10.66.9.46;Database=BD_ML;Trusted_Connection=yes;"))
 
+# Sigla NOVA, apenas insert sem drop...
+# caminho <- list.files(
+#   "\\\\spo-leste60_fs\\FISCALIZAÇÃO\\FAC FAR\\BANCO DE DADOS ML\\OLM",
+#   pattern = "*.xlsx", full.names = T)
+
+
+# Arquivo atual
+# último arquivo -> 02/06/2024
+caminho <- "\\\\spo-leste60_fs\\FISCALIZAÇÃO\\FAC FAR\\BANCO DE DADOS ML\\OLM\\7 - 27 a 02.junho.24.xlsx"
+
+dt1 <- lapply(caminho, function(x) read_excel(x, sheet = 1)) |> bind_rows()
+dt2 <- lapply(caminho, function(x) read_excel(x, sheet = 2)) |> bind_rows()
+
+mapped_relatorios <- dt1 %>%
+  rename(!!!col_mapping_relatorios)
+
+mapped_formularios <- dt2 %>%
+  rename(!!!col_mapping_formularios)
+
+formularios_filtrado <- mapped_formularios |> 
+  filter(DESCRICAO == "UTILIZACAO CORRETA DO PDA" 
+         | DESCRICAO == "PAVIMENTACAO DE ACORDO ESPECIFICACAO TEC"
+  )
+
+
+# Insert 
+str_formulario <- as.data.frame(lapply(formularios_filtrado, as.character))
+dbWriteTable(conexao,
+             name = SQL('"LESTE_AD\\hcruz_novasp"."tbHyslancruz_Formularios_OLM_FACFAR"'),
+             value = str_formulario,
+             overwrite = FALSE,
+             append = TRUE,
+)
+
+
+str_relatorio <- as.data.frame(lapply(mapped_relatorios, as.character))
+str_relatorio <- str_relatorio |> 
+  mutate(DT_COMPETENCIA = as.POSIXct(DT_COMPETENCIA, format = "%d/%m/%Y %H:%M"),
+         DT_PLANEJAMENTO = as.POSIXct(DT_PLANEJAMENTO, format = "%d/%m/%Y %H:%M"),
+         DT_INICIO_EXEC = as.POSIXct(DT_INICIO_EXEC, format = "%d/%m/%Y %H:%M"),
+         DT_FIM_EXEC_OS_FISCALIZADA = as.POSIXct(DT_FIM_EXEC_OS_FISCALIZADA, format = "%d/%m/%Y %H:%M"),
+         DT_COMPETENCIA_OS_FISCALIZADA = as.POSIXct(DT_COMPETENCIA_OS_FISCALIZADA, format = "%d/%m/%Y %H:%M"),
+         DT_PLANEJAMENTO_OS_FISCALIZADA = as.POSIXct(DT_PLANEJAMENTO_OS_FISCALIZADA, format = "%d/%m/%Y %H:%M"),
+         DT_INICIO_EXEC_OS_FISCALIZADA = as.POSIXct(DT_INICIO_EXEC_OS_FISCALIZADA, format = "%d/%m/%Y %H:%M"),
+         DT_FIM_EXECUCAO = as.POSIXct(DT_FIM_EXECUCAO, format = "%d/%m/%Y %H:%M"))
+
+dbWriteTable(conexao,
+             name = SQL('"LESTE_AD\\hcruz_novasp"."tbHyslancruz_RelatorioOS_OLM_FACFAR"'),
+             value = str_relatorio,
+             overwrite = FALSE,
+             append = TRUE,
+)
+
+### Modelo antigo
 # Executa Procedure
 dbExecute(conexao,
           "
-DELETE FROM [LESTE_AD\\hcruz_novasp].[tbHyslancruz_RelatorioOS_MLG_FACFAR]
-DELETE FROM [LESTE_AD\\hcruz_novasp].[tbHyslancruz_RelatorioOS_MLN_FACFAR]
-DELETE FROM [LESTE_AD\\hcruz_novasp].[tbHyslancruz_RelatorioOS_MLQ_FACFAR]
-DELETE FROM [LESTE_AD\\hcruz_novasp].[tbHyslancruz_Formularios_MLG_FACFAR]
-DELETE FROM [LESTE_AD\\hcruz_novasp].[tbHyslancruz_Formularios_MLN_FACFAR]
-DELETE FROM [LESTE_AD\\hcruz_novasp].[tbHyslancruz_Formularios_MLQ_FACFAR]
+EXEC [LESTE_AD\\hcruz_novasp].[P_BANCO_FACFAR]
 ")
 
 #Planilhas Excel
@@ -36,7 +142,7 @@ formularios_mlq <- read_excel(caminho_mlq, sheet = "Formulário")
 
 # Mapeamento das colunas
 col_mapping_formularios <- c(
-   NUM_OS = "Número OS"
+  NUM_OS = "Número OS"
   ,COD_TSS_OS_FISCALIZADA = "Cod. TSS OS Fiscalizada"
   ,CAUSA_RESULTADO_OS_FISCALIZADA = "Causa Resultado OS Fiscalizada"
   ,GRUPO = "Grupo"
@@ -189,4 +295,4 @@ dbWriteTable(conexao,
              overwrite = FALSE,
              append = TRUE,
 )
-
+print("Dados inseridos com sucesso!")
